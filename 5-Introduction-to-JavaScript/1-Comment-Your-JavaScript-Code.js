c1cfa294d28e870cff67cb24fcfe52311aa0ddd9
DevDenver <michaelmtm2016@gmail.com>
2019-06-02 16:50:00 -0600

    Correct syntax for in-line comment

--- 5-Introduction-to-JavaScript/1-Comment-Your-JavaScript-Code.js
+++ 5-Introduction-to-JavaScript/1-Comment-Your-JavaScript-Code.js
@@ -1,2 +1,2 @@
-// I learned the two main ways of documenting code in javaScript This is an in-line comment //
-/* The two ways are in line comment and multi- line comments.  The in line has a double slash tags at the beginning and end of a single line.  A multi-line comment has the slash and star tag and closes with the star slash tag. */
+// I learned the two main ways of documenting code in javaScript This is an in-line comment 
+/* The two ways are in line comment and multi- line comments.  The in line has a double slash tags at the beginning of a single line.  A multi-line comment has the slash and star tag and closes with the star slash tag. */
