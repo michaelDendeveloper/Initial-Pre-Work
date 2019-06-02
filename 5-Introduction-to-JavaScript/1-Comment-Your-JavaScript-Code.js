281681596eb85405fa14f4c7a656b3c556d8a7f9
DevDenver <michaelmtm2016@gmail.com>
2019-06-02 16:41:25 -0600

    Add single-line and multi-line comments using appropriate tags

--- 5-Introduction-to-JavaScript/1-Comment-Your-JavaScript-Code.js
+++ 5-Introduction-to-JavaScript/1-Comment-Your-JavaScript-Code.js
@@ -1,3 +1,2 @@
-// Please remember to include a summary statement with EACH FILE regarding what you learned from the lesson in this format!  I recommend keeping it at the top of each file for faster processing of your application.
-
-// Copy your code from the central panel of the freeCodeCamp page and paste it below after successfully passing the test.  You will have to click on the X at the top right of the modal that pops up to see the code. If you forget, just rerun the test.  You are also welcome to download your solution.
+// I learned the two main ways of documenting code in javaScript This is an in-line comment //
+/* The two ways are in line comment and multi- line comments.  The in line has a double slash tags at the beginning and end of a single line.  A multi-line comment has the slash and star tag and closes with the star slash tag. */
