92c7ae63d7bcf81aaa930d209b11a95ee601f3de
DevDenver <michaelmtm2016@gmail.com>
2019-06-03 16:33:49 -0600

    Concatenate string and string variable using +=

---
+++ 5-Introduction-to-JavaScript/28-Appending_Variables_to_Strings.js
@@ -0,0 +1,11 @@
+/* Examle I learned how to append variables to strings using the plus equals operator. In this case, the string is built by using the plus equals with a string variable to concatenate strings. */
+
+var anAdjective = "awesome!";
+var ourStr = "freeCodeCamp is ";
+ourStr += anAdjective;
+
+// Only change code below this line
+
+var someAdjective = "fun.";
+var myStr = "Learning to code is ";
+myStr += someAdjective;
