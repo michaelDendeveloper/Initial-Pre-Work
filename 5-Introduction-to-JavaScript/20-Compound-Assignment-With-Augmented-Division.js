cba1267070ccd7e401d99722b3a373f10eec034b
DevDenver <michaelmtm2016@gmail.com>
2019-06-03 14:58:03 -0600

    Add comment after test shows compound assignment augmented division works

---
+++ 5-Introduction-to-JavaScript/20-Compound-Assignment-With-Augmented-Division.js
@@ -0,0 +1,10 @@
+//I learned that the compound assignment /= can be used for augmented division.
+var a = 48;
+var b = 108;
+var c = 33;
+
+// Only modify code below this line
+
+a /= 12;
+b /= 4;
+c /= 11;
