4977dae3dc0e6a3fb57b57f7f7d8122fe46a4e82
DevDenver <michaelmtm2016@gmail.com>
2019-06-03 13:47:02 -0600

    Change all Variable names to camelCase

---
+++ 5-Introduction-to-JavaScript/6-Understanding-Case-Sensitivity-in-Variables.js
@@ -0,0 +1,10 @@
+// Declarations I learned that all variables and all names are case sensitive in JavaScript. Best practice
+//use camelCase
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
