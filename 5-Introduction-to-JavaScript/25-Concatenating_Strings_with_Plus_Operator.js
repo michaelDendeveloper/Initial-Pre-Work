e1076d1e8663957ffbd2dffb740fb17c8c8a8289
DevDenver <michaelmtm2016@gmail.com>
2019-06-03 15:56:02 -0600

    Concatenate two strings with the Plus operator

---
+++ 5-Introduction-to-JavaScript/25-Concatenating_Strings_with_Plus_Operator.js
@@ -0,0 +1,6 @@
+// Example I learned how to concatenate strings with the plus operator.
+var ourStr = "I come first. " + "I come second.";
+
+// Only change code below this line
+
+var myStr = "This is the start. " + "This is the end.";
