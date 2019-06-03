8fec0a66e094bce6a59b991133829be7ac352e8e
DevDenver <michaelmtm2016@gmail.com>
2019-06-03 15:52:24 -0600

    Insert escape characters to format three lines for output

---
+++ 5-Introduction-to-JavaScript/24-Escape-Sequences-in-Strings.js
@@ -0,0 +1,3 @@
+/* I learned There is a list of escape characters.  \',\",\\,\n,\r,\t,\b,\f.  The backslash itself must be escaped in order to display as a backslash.  Escape sequences are necessary to insert special characters correctly.  Do not include spaces between escape sequences or words"
+*/
+var myStr = "FirstLine\n\t\\SecondLine\nThirdLine"; // Change this line
