498e83556282c25369947f79518d478da0dc7167
DevDenver <michaelmtm2016@gmail.com>
2019-06-03 14:41:56 -0600

    replaced assignment with augmented multiplication

---
+++ 5-Introduction-to-JavaScript/19-Compound-Assignment-With-Augmented-Multiplication.js
@@ -0,0 +1,10 @@
+// I learned that the augmented multiplication *= is similar to augmented addition and subtraction
+var a = 5;
+var b = 12;
+var c = 4.6;
+
+// Only modify code below this line
+
+a *= 5;
+b *= 3;
+c *= 10;
