402fba94bf49584eabbc8ec6face8246e96294ca
DevDenver <michaelmtm2016@gmail.com>
2019-06-03 16:50:18 -0600

    Return first letter of string using index

---
+++ 5-Introduction-to-JavaScript/30-Use_Bracket_Notation_to_Find_the_First_Character_in_a_String.js
@@ -0,0 +1,12 @@
+/* Example I learned that to find the first character in a string, use the bracket notation. Strings in Javascript use zero-based indexing.  The index of the first character in the string array is zero.  Bracket notaion is the same as indexing position numbers in arrays. */
+var firstLetterOfFirstName = "";
+var firstName = "Ada";
+
+firstLetterOfFirstName = firstName[0];
+
+// Setup
+var firstLetterOfLastName = "";
+var lastName = "Lovelace";
+
+// Only change code below this line
+firstLetterOfLastName = lastName[0];
