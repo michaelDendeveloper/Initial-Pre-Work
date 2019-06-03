5f67de66d0062be02aa56f1898342c5297cfd99d
DevDenver <michaelmtm2016@gmail.com>
2019-06-03 16:19:20 -0600

    build a string with strings, concatenation operators and variables

---
+++ 5-Introduction-to-JavaScript/27-Constructing_Strings_with_Variables.js
@@ -0,0 +1,7 @@
+// Example. I learned that I can use a string variable with concatenation operators to combine strings
+var ourName = "freeCodeCamp";
+var ourStr = "Hello, our name is " + ourName + ", how are you?";
+
+// Only change code below this line
+var myName = "Michael";
+var myStr = "My name is " + myName + " and I am well!";
