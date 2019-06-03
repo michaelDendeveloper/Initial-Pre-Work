2e67e515344a1a8156e543e99eeb65b29a0a39fb
DevDenver <michaelmtm2016@gmail.com>
2019-06-03 17:50:19 -0600

    Replace index for first element of string with replacement string

---
+++ 5-Introduction-to-JavaScript/31-Understand_String_Immutability.js
@@ -0,0 +1,10 @@
+/* Setup I learned that strings are immutable.  Immutable means they cannot be altered once created.
+This does not mean the string cannot be changed, it means that individual characters of a string
+literal cannot be changed.  Do not correct misspellings, assign it with a new string.
+*/
+
+var myStr = "Jello World";
+
+// Only change code below this line
+
+myStr = "Hello World"; // Fix Me
