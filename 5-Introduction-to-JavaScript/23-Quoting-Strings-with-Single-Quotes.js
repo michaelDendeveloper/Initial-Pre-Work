f5d7c8e78a8bc2956dfe4e609ad6c0b3fe417845
DevDenver <michaelmtm2016@gmail.com>
2019-06-03 15:40:53 -0600

    replace escape quotes with quotes that begin and end the same

---
+++ 5-Introduction-to-JavaScript/23-Quoting-Strings-with-Single-Quotes.js
@@ -0,0 +1,2 @@
+/* I learned that string values may be written in javaScript with single or double quotes if the string starts and ends with the same type of quote.  In JavaScript, single and double quotes work the same.
+var myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
