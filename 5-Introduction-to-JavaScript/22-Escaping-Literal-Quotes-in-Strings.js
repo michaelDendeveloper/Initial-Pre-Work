742a07b93bfdcc0c7acaff6aa9513e3395922709
DevDenver <michaelmtm2016@gmail.com>
2019-06-03 15:26:16 -0600

    Include escape quotes inside quotes in string

---
+++ 5-Introduction-to-JavaScript/22-Escaping-Literal-Quotes-in-Strings.js
@@ -0,0 +1,2 @@
+//I learned that you can escape a quote from considering it the end of a string by using \ in front of the quote
+var myStr = "I am a \"double quoted\" string inside \"double quotes\"."; //Change this line
