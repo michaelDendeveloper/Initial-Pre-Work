b77ec12034750611def22af482ddf130c01591cc
DevDenver <michaelmtm2016@gmail.com>
2019-06-03 15:02:24 -0600

    Create String literals and assign values

---
+++ 5-Introduction-to-JavaScript/21-Declare-String-Variables.js
@@ -0,0 +1,7 @@
+// Example I learned that a string literal is zero or more characters enclosed in single or double quotes
+var firstName = "Alan";
+var lastName = "Turing";
+
+// Only change code below this line
+var myFirstName = "Michael"
+var myLastName = "McLoughlin"
