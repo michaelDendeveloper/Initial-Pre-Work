35272dbdc8bc535856c1f7698e92a9e1451f4b4e
DevDenver <michaelmtm2016@gmail.com>
2019-06-03 14:27:53 -0600

    Add comment after test.  replace assignment with compound assignment

---
+++ 5-Introduction-to-JavaScript/17-Compound-Assignment-With-Augmented-Addition.js
@@ -0,0 +1,10 @@
+/* I learned that everything to the right of the assignment operator is evaluated first.  Since this is such a common pattern, there are operators that do a mathematical operation and assignment in one step.  One example of this type of operator is a += operator.  Use this to cut down length of lines*/
+var a = 3;
+var b = 17;
+var c = 12;
+
+// Only modify code below this line
+
+a += 12;
+b += 9;
+c += 7;
