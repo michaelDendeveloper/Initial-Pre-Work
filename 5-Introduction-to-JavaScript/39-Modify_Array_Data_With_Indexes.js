7d82ea35bca81eddc7a32a3c93f9a9649aa58035
DevDenver <michaelmtm2016@gmail.com>
2019-06-03 19:12:53 -0600

    Modify 0 element of myArray to 45

---
+++ 5-Introduction-to-JavaScript/39-Modify_Array_Data_With_Indexes.js
@@ -0,0 +1,10 @@
+/* Example I learned that the entries of arrays are mutable, which means that they can be changed freely.  To keep the code easy to read, it is best practice to use square brackets without a space between the name and the square brackets.
+*/
+var ourArray = [18,64,99];
+ourArray[1] = 45; // ourArray now equals [18,45,99].
+
+// Setup
+var myArray = [18,64,99];
+
+// Only change code below this line.
+myArray[0] = 45;
