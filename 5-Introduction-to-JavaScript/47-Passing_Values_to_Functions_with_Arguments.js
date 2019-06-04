f9b9daaf4b2b571b3246dc578cc2ccf6d5128e08
DevDenver <michaelmtm2016@gmail.com>
2019-06-04 10:54:33 -0600

    Create function with two parameters that ouputs the sum of the numbers passed in

---
+++ 5-Introduction-to-JavaScript/47-Passing_Values_to_Functions_with_Arguments.js
@@ -0,0 +1,13 @@
+/* I learned that values are passed to functions are arguments.  Arguments are the actual values that are used as input in a function. Parameters are the variables that are placeholders that are to be input into a function.  The parameters are usually defined when the function is defined.
+*/
+// Example
+function ourFunctionWithArgs(a, b) {
+  console.log(a - b);
+}
+ourFunctionWithArgs(10, 5); // Outputs 5
+
+// Only change code below this line.
+function functionWithArgs(firstvariable,secondvariable){
+  console.log(firstvariable+secondvariable);
+}
+functionWithArgs(5,4);
