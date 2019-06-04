f0b1fd26d4883393e98e31b51260a7efd8f3e1f4
DevDenver <michaelmtm2016@gmail.com>
2019-06-03 18:09:13 -0600

    Use string length minus one as the index

---
+++ 5-Introduction-to-JavaScript/33-Use_Bracket_Notation_to_Find_the_Last_Character_in_a_String.js
@@ -0,0 +1,9 @@
+/* Example I learned that to locate the last character in the string, subtract one from the string's length, the use the result for the index */
+var firstName = "Ada";
+var lastLetterOfFirstName = firstName[firstName.length - 1];
+
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line.
+var lastLetterOfLastName = lastName[lastName.length - 1];
