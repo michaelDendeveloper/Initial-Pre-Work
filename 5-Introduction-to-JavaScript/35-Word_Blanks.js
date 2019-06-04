49624f0a072f641695bce8f222145447efcc2a22
DevDenver <michaelmtm2016@gmail.com>
2019-06-03 18:48:17 -0600

    Add line with string operators in function

---
+++ 5-Introduction-to-JavaScript/35-Word_Blanks.js
@@ -0,0 +1,11 @@
+/* I learned that the string manipulators that are available in javaScript can be combined to format output. This challenge uses a definition of a function and four parameters
+*/
+// Your code below this line
+var result = "";
+result = "I took my " + myNoun + " to the " + myAdjective + " dogpark and he " + myVerb + " around " + myAdverb;
+// Your code above this line
+return result;
+}
+
+// Change the words here to test your function
+wordBlanks("dog", "big", "ran", "quickly");
