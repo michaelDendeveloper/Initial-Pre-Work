43bbafbd465c6c15163d3d1230ae12a76db26119
DevDenver <michaelmtm2016@gmail.com>
2019-06-03 18:54:39 -0600

    Declare array with string and number elements

---
+++ 5-Introduction-to-JavaScript/36-Store_Multiple_Values_in_one_Variable_using_JavaScript_Arrays.js
@@ -0,0 +1,5 @@
+/* Example I learned to use Arrays in JavaScript.  Begin by declaring the array variable with elements of the array in square brackets. Strings and numbers can both be used in arrays.  */
+var ourArray = ["John", 23];
+
+// Only change code below this line.
+var myArray = ["peanut butter", 10];
