cc6b044e582b3fc79e0ed1aaae127c0be9586707
DevDenver <michaelmtm2016@gmail.com>
2019-06-03 18:13:48 -0600

    use index to return nth from last character in a string

---
+++ 5-Introduction-to-JavaScript/34-Use_Bracket_Notation_to_Find_the_Nth-to-Last_Character_in_a_String.js
@@ -0,0 +1,9 @@
+// Example I learned that the Nth to last character in a string can be found with an index in bracket notation
+var firstName = "Ada";
+var thirdToLastLetterOfFirstName = firstName[firstName.length - 3];
+
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line
+var secondToLastLetterOfLastName = lastName[lastName.length-2];
