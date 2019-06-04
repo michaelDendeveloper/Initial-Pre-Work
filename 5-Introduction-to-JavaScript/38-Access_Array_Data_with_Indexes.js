2e62e2f17ed2f9141232d1182e5df7a5be1ebceb
DevDenver <michaelmtm2016@gmail.com>
2019-06-03 19:07:00 -0600

    Assign value of zero element of myArray to variable MyData

---
+++ 5-Introduction-to-JavaScript/38-Access_Array_Data_with_Indexes.js
@@ -0,0 +1,10 @@
+/* Example I learned the method of using square brackets to access elements of string arrays can be used to specify an entry in an array. Strings are arrays, so arrays in general use zero-based indexing.  The first element of an array is element 0. There shouldn't be any spaces between the array name and the square brackets, it makes the code confusing to read
+*/
+
+var ourArray = [50,60,70];
+var ourData = ourArray[0]; // equals 50
+
+// Setup
+var myArray = [50,60,70];
+var myData = myArray[0];
+// Only change code below this line.
