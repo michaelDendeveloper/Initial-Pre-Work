0f31c9610876ba764ff1183f5140951bfa2a06f8
DevDenver <michaelmtm2016@gmail.com>
2019-06-04 10:36:29 -0600

    Create multi-dimensional array with five elements, each with a string and number

---
+++ 5-Introduction-to-JavaScript/45-Shopping_List.js
@@ -0,0 +1,3 @@
+/* I learned how to create a multi-dimensional arrays containing sub arrays using javaScript commands for creating multi-dimensional arrays.
+*/
+var myList = [["Chocolate Bar",15],["eggs",12],["Bagels",12],["Doughnuts",12],["Almonds",121]];
