24fbd94218b793e80fa37f6720d75a3a8d9510fd
DevDenver <michaelmtm2016@gmail.com>
2019-06-03 18:58:44 -0600

    declare multi-dimensional array myArray

---
+++ 5-Introduction-to-JavaScript/37-Nest_one_Array_within_Another_Array.js
@@ -0,0 +1,5 @@
+// Example I learned that in javaScript, arrays can be nested within arrays creating multi-dimensional arrays
+var ourArray = [["the universe", 42], ["everything", 101010]];
+
+// Only change code below this line.
+var myArray = [[ourArray],"peanut butter"];
