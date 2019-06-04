fc2b812e662958cdfc628da92b043914ecb2a22e
DevDenver <michaelmtm2016@gmail.com>
2019-06-04 11:29:45 -0600

    Create a function returning five multiplied by the value passed into the function

---
+++ 5-Introduction-to-JavaScript/51-Return_a_Value_from_a_Function_with_Return.js
@@ -0,0 +1,14 @@
+/* I learned the use of arguments and return statements. Arguments are the values passed into the function, the return statement is used to send the result of the function operations out of the function. I created a function that multiplies vaule sent to it by five and returns the result.
+*/
+// Example
+function minusSeven(num) {
+  return num - 7;
+}
+
+// Only change code below this line
+function timesFive(multiOne){
+  return(multiOne*5);
+}
+console.log(timesFive(5));
+
+console.log(minusSeven(10));
