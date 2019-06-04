97d3d0176c33cb48863d8b610c9d30e3e9b652e5
DevDenver <michaelmtm2016@gmail.com>
2019-06-03 19:35:56 -0600

    use push() function to add values to multi-dimensional array

---
+++ 5-Introduction-to-JavaScript/41-Manipulate_Arrays_With_push().js
@@ -0,0 +1,12 @@
+// Example I learned that the push() function is an easy way to append data to an end of an array
+/* for multi-dimensional arrays, the square brackets are in the parentheses in the function.  Use dot operator to call the function.
+*/
+var ourArray = ["Stimpson", "J", "cat"];
+ourArray.push(["happy", "joy"]);
+// ourArray now equals ["Stimpson", "J", "cat", ["happy", "joy"]]
+
+// Setup
+var myArray = [["John", 23], ["cat", 2]];
+
+// Only change code below this line.
+myArray.push(["dog", 3]);
