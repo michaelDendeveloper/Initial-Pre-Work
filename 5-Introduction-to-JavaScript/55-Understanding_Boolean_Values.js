d489b97f96e58a8f8e29636a86cc7426e3cabcc1
DevDenver <michaelmtm2016@gmail.com>
2019-06-04 12:49:58 -0600

    Changed boolean value from false to true

---
+++ 5-Introduction-to-JavaScript/55-Understanding_Boolean_Values.js
@@ -0,0 +1,10 @@
+/* I learned the boolean type is another data type in javaScript.  Boolean data may be one of two values: true or false.  The two values are mutually exclusive.  Boolean values are not strings, so not written in quotes.
+*/
+function welcomeToBooleans() {
+
+// Only change code below this line.
+
+return true; // Change this line
+
+// Only change code above this line.
+}
