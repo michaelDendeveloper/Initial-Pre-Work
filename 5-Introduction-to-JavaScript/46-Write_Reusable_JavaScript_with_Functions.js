db55dd5bed8a101507b4f5b1003751b1f94106f9
DevDenver <michaelmtm2016@gmail.com>
2019-06-04 10:46:13 -0600

    Create function reusableFunction(), call function once

---
+++ 5-Introduction-to-JavaScript/46-Write_Reusable_JavaScript_with_Functions.js
@@ -0,0 +1,14 @@
+/* I learned that functions can be used to create reusable code.  Functions can be created and then invoked, or called by the function name followed by parentheses. I learned to create a function by creating a function called reusableFunction.
+*/
+// Example
+function ourReusableFunction() {
+  console.log("Heyya, World");
+}
+
+ourReusableFunction();
+
+// Only change code below this line
+function reusableFunction() {
+  console.log("Hi World");
+}
+reusableFunction();
