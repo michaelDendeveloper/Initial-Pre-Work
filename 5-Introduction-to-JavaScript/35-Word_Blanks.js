38233cf1323e4944e6f8a52b8825c1ddeae2850e
DevDenver <michaelmtm2016@gmail.com>
2019-06-03 18:49:31 -0600

    add line that contains string manipulations to function wordBlanks

--- 5-Introduction-to-JavaScript/35-Word_Blanks.js
+++ 5-Introduction-to-JavaScript/35-Word_Blanks.js
@@ -1,11 +1,12 @@
 /* I learned that the string manipulators that are available in javaScript can be combined to format output. This challenge uses a definition of a function and four parameters
 */
-// Your code below this line
-var result = "";
-result = "I took my " + myNoun + " to the " + myAdjective + " dogpark and he " + myVerb + " around " + myAdverb;
-// Your code above this line
-return result;
+function wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
+  // Your code below this line
+  var result = "";
+result = "My striped " + myNoun + " went to a " + myAdjective + " park and then " + myVerb + " the park bench " + myAdverb;
+  // Your code above this line
+  return result;
 }
 
 // Change the words here to test your function
-wordBlanks("dog", "big", "ran", "quickly");
+wordBlanks("cat", "little", "hit", "quickly");
