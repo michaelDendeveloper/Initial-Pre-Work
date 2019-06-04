a40624838b367b58746cb5f62cbcd31de9362dc7
DevDenver <michaelmtm2016@gmail.com>
2019-06-03 18:02:30 -0600

    Add index to locate third letter in the string

---
+++ 5-Introduction-to-JavaScript/32-Use_Bracket_Notation_to_Find_the_Nth_Character_in_a_String.js
@@ -0,0 +1,10 @@
+/* Example I learned that bracket notation can return a character in other positions within a string
+*/
+var firstName = "Ada";
+var secondLetterOfFirstName = firstName[1];
+
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line.
+var thirdLetterOfLastName = lastName[2];
