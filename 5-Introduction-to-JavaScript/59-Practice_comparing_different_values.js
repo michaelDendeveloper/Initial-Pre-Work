ffa1ad0f6dbb22e0c83802ff503420e4f650bded
DevDenver <michaelmtm2016@gmail.com>
2019-06-04 15:05:04 -0600

    Change equality operator to strict equality operator in function compareEquality

---
+++ 5-Introduction-to-JavaScript/59-Practice_comparing_different_values.js
@@ -0,0 +1,12 @@
+/* I learned that typeof operator returns the type of a variable, so it can be used to determine variable types.
+*/
+// Setup
+function compareEquality(a, b) {
+  if (a === b) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+// Change this value to test
+compareEquality(10, "10");
