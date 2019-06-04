603056871ac4fe49653e8e31fc94de53539d81d6
DevDenver <michaelmtm2016@gmail.com>
2019-06-03 19:43:03 -0600

    Use .pop() functions to return value of element in multi-dimensional array

---
+++ 5-Introduction-to-JavaScript/42-Manipulate_Arrays_With_pop().js
@@ -0,0 +1,12 @@
+/* I learned how to use the .pop() function to change data in an array.  .pop() is used to pop a value off the end of an array.  The value that is "popped off" can be stored by assigning the value to a variable.  The .pop() function removes the last element of an array and returns that element
+*/
+// Example
+var ourArray = [1,2,3];
+var removedFromOurArray = ourArray.pop();
+// removedFromOurArray now equals 3, and ourArray now equals [1,2]
+
+// Setup
+var myArray = [["John", 23], ["cat", 2]];
+
+// Only change code below this line.
+var removedFromMyArray = myArray.pop();
