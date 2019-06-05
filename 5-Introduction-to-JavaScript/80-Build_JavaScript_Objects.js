d4cb7336325982dc73bd646abad88571224e41e3
DevDenver <michaelmtm2016@gmail.com>
2019-06-05 11:42:07 -0600

    Create object with number and string data

---
+++ 5-Introduction-to-JavaScript/80-Build_JavaScript_Objects.js
@@ -0,0 +1,20 @@
+/* I learned to create objects in javaScript.  The data in objects is accessed through properties, which is how they differ from arrays, where data are accessed through indexes.  Objects are useful for representing real world objects that have recordable attributes, such as cats and dogs.  Objects can contain numbers and strings.
+*/
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+// Only change code below this line.
+
+var myDog = {
+"name":"fido",
+"legs": 4,
+"tails": 1,
+"friends":["Camper"]
+
+
+};
