a8beb4254519b7f81bf367f82fe6b67e5b868e10
DevDenver <michaelmtm2016@gmail.com>
2019-06-04 19:51:53 -0600

    Add test for values less than zero, return to escape function when true

---
+++ 5-Introduction-to-JavaScript/78-Return_Early_Pattern_for_Functions.js
@@ -0,0 +1,20 @@
+/*I learned a return statement will stop the current function and return control to the calling location
+*/
+// Setup
+function abTest(a, b) {
+  // Only change code below this line
+  if(a<0){
+    return undefined;
+  }
+  if(b <0){
+    return undefined;
+  }
+
+
+  // Only change code above this line
+
+  return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+}
+
+// Change values below to test your code
+abTest(2,2);
