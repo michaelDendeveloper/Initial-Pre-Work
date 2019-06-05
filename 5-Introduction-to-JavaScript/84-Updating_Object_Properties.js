152b0e57ccdb46d4386d5909e5848ef29afe98d4
DevDenver <michaelmtm2016@gmail.com>
2019-06-05 12:29:38 -0600

    Update properties of object myDog with dot notation

---
+++ 5-Introduction-to-JavaScript/84-Updating_Object_Properties.js
@@ -0,0 +1,23 @@
+/* I learned that the properties of an object can be updated in the same way variables are updated. Either dot or bracket notation can be updated.
+*/
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+ourDog.name = "Happy Camper";
+
+// Setup
+var myDog = {
+  "name": "Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+
+// Only change code below this line.
+myDog.name = "Happy Coder";
+console.log(myDog.name);
