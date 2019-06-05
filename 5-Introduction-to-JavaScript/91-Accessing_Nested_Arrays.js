2f046b9645dc64f5c18ec686264934ec97856ef1
DevDenver <michaelmtm2016@gmail.com>
2019-06-05 14:52:01 -0600

    Access data in complex object with nested brackets and dot notation

---
+++ 5-Introduction-to-JavaScript/91-Accessing_Nested_Arrays.js
@@ -0,0 +1,26 @@
+/* I learned that the bracket notation can be nested in statements to access properties of complex objects.
+*/
+// Setup
+var myPlants = [
+  {
+    type: "flowers",
+    list: [
+      "rose",
+      "tulip",
+      "dandelion"
+    ]
+  },
+  {
+    type: "trees",
+    list: [
+      "fir",
+      "pine",
+      "birch"
+    ]
+  }
+];
+
+// Only change code below this line
+
+var secondTree = myPlants[1].list[1]; // pine
+console.log(secondTree);
