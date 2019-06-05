fc54be53aadda9b0dd52f927296181ce24d8ca35
DevDenver <michaelmtm2016@gmail.com>
2019-06-04 19:36:32 -0600

    replace nested if else chains with switch in the function

---
+++ 5-Introduction-to-JavaScript/76-Replacing_If_Else_Chains_with_Switch.js
@@ -0,0 +1,28 @@
+/* I learned to write a function with a switch statement to replace nested conditional statements
+*/
+function chainToSwitch(val) {
+  var answer = "";
+  // Only change code below this line
+  switch(val){
+    case "bob":
+    answer = "Marley";
+    break;
+    case 42:
+    answer = "The Answer";
+    break;
+    case 1:
+    answer = "There is no #1";
+    break;
+    case 99:
+    answer = "Missed me by this much!";
+    break;
+    case 7:
+    answer = "Ate Nine";
+  }
+
+  // Only change code above this line
+  return answer;
+}
+
+// Change this value to test
+chainToSwitch(7);
