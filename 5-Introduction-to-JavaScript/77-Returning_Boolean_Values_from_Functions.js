670e9dde177e6fffb7177972633e30a5c0e3caf4
DevDenver <michaelmtm2016@gmail.com>
2019-06-04 19:45:27 -0600

    Replace if statements with comparison, return only boolean value from formula

---
+++ 5-Introduction-to-JavaScript/77-Returning_Boolean_Values_from_Functions.js
@@ -0,0 +1,8 @@
+/*I learned that comparisons can be used to replace comparison statements to reduce code
+*/
+function isLess(a, b) {
+  // Fix this code
+  return a < b;
+}
+// Change these values to test
+isLess(10, 15);
