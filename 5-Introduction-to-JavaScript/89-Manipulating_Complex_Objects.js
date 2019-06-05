45079577849f3114b68be378acd34feb000d8151
DevDenver <michaelmtm2016@gmail.com>
2019-06-05 14:17:44 -0600

    Create new data in complex object.

---
+++ 5-Introduction-to-JavaScript/89-Manipulating_Complex_Objects.js
@@ -0,0 +1,28 @@
+/* I learned that a JavaScript object is one way to handle flexible data.   Objects allow for arbritrary combinations of strings, numbers, booleans, arrays, functions and objects. JavaScript Object Notation (JSON) is a related data interchange format used to store data.  A comma is placed after every object in the array, unless it is the last object in the array.
+*/
+
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  // Add record here
+  {
+    "artist":"Pink Floyd",
+    "title": "The division bell",
+    "release_year": 1994,
+    "formats": [
+      "CD",
+      "LP",
+      "IT"
+    ],
+    "gold": true
+  }
+];
