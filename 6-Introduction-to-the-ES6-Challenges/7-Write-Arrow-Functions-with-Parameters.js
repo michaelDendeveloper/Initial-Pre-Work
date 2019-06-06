694f3bb69a60abc6cfbc4b9c61b4d0108ef26071
DevDenver <michaelmtm2016@gmail.com>
2019-06-06 16:19:59 -0600

    Change function to arrow notation

---
+++ 6-Introduction-to-the-ES6-Challenges/7-Write-Arrow-Functions-with-Parameters.js
@@ -0,0 +1,8 @@
+/*I learned that arguments can be passed as parameters.  This was changed to arrow notation
+*/
+const myConcat = (arr1, arr2) => {
+  "use strict";
+  return arr1.concat(arr2);
+};
+// test your code
+console.log(myConcat([1, 2], [3, 4, 5]));
