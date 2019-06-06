323fd4c977c7c66ab908a578a6f1c70e649bbd6c
DevDenver <michaelmtm2016@gmail.com>
2019-06-06 10:17:35 -0600

    Add loop to populate array with odd numbers in descending order

--- 5-Introduction-to-JavaScript/96-Count_Backwards_With_a_For_Loop.js
+++ 5-Introduction-to-JavaScript/96-Count_Backwards_With_a_For_Loop.js
@@ -1,1 +1,17 @@
+/*I learned that a loop can count backwards by defining the right conditions in the initialization, condition and final-expression of the loop definition. In this example, odd numbers from 9 counting down to 1 are pushed to myArray
+*/
+// Example
+var ourArray = [];
 
+for (var i = 10; i > 0; i -= 2) {
+  ourArray.push(i);
+}
+
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+for (var i=9; i > 0; i-=2){
+  myArray.push(i);
+  console.log(myArray);
+}
