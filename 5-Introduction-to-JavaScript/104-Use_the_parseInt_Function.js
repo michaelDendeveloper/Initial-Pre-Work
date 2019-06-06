8c95407be663f294a603ce6709a53da7978d6ac1
DevDenver <michaelmtm2016@gmail.com>
2019-06-06 14:59:09 -0600

    use parseInt() function in convertToInteger to parse strings to equvalent integers

---
+++ 5-Introduction-to-JavaScript/104-Use_the_parseInt_Function.js
@@ -0,0 +1,8 @@
+/*I learned to use the parseInt() function to parse a string to return an integer.
+*/
+function convertToInteger(str) {
+  return parseInt(str);
+
+}
+
+console.log(convertToInteger("jamesBond"));
