a88b921b7fe4da566ae30dd332307ef27bdc3f13
DevDenver <michaelmtm2016@gmail.com>
2019-06-06 18:33:26 -0600

    reduce lines of code writing simple fields using concise object literal declarations

---
+++ 6-Introduction-to-the-ES6-Challenges/18-Write-Concise-Object-Literal-Declarations-Using-Simple-Fields.js
@@ -0,0 +1,9 @@
+// I learned to write concise object literal declarations using simple fields eliminating redundancy
+const createPerson = (name, age, gender) => {
+  "use strict";
+  // change code below this line
+  return ({ name, age, gender})
+
+  // change code above this line
+};
+console.log(createPerson("Zodiac Hasbro", 56, "male")); // returns a proper object
