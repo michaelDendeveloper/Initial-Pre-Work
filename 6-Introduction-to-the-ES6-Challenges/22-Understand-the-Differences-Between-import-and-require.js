87e42467f4183cd3758f0ca1f997725c3468aa28
DevDenver <michaelmtm2016@gmail.com>
2019-06-06 19:21:45 -0600

    Add import statement

---
+++ 6-Introduction-to-the-ES6-Challenges/22-Understand-the-Differences-Between-import-and-require.js
@@ -0,0 +1,5 @@
+/*I learned to use import to use import tool to choose which part of a module to load
+*/
+"use strict";
+import  {capitalizeString} from "string_functions";
+capitalizeString("hello!");
