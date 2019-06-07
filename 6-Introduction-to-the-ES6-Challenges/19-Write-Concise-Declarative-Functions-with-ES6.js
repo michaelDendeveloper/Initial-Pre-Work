d83ae51cb2451d163431cafb8f7753aa44491cf1
DevDenver <michaelmtm2016@gmail.com>
2019-06-06 18:39:24 -0600

    removed function keyword from declaration as supported by ES5

---
+++ 6-Introduction-to-the-ES6-Challenges/19-Write-Concise-Declarative-Functions-with-ES6.js
@@ -0,0 +1,13 @@
+/* I learned that in ES5 the keyword can be removed from declarations for reusable blocks of code
+*/
+// change code below this line
+const bicycle = {
+  gear: 2,
+  setGear(newGear) {
+    "use strict";
+    this.gear = newGear;
+  }
+};
+// change code above this line
+bicycle.setGear(3);
+console.log(bicycle.gear);
