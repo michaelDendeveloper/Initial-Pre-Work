5f04ae51eedc8caebd229dbd9d14d3c3fd696202
DevDenver <michaelmtm2016@gmail.com>
2019-07-29 17:50:53 -0600

    define a constructor and use keyword to instantiate a new instance of the object

--- 6-Introduction-to-the-ES6-Challenges/20-Use-class-Syntax-to-Define-a-Constructor-Function.js
+++ 6-Introduction-to-the-ES6-Challenges/20-Use-class-Syntax-to-Define-a-Constructor-Function.js
@@ -1,15 +1,13 @@
 /* ES6 provides new syntax to help create objects.  Use the keyword class. This does not create a class as in object-oriented programming, it is just a syntax.
-*/
+The correct sequence is to define a constructor function, then use the use the new keyword to instantiate a new instance  of the object*/
 function makeClass() {
   "use strict";
   /* Alter code below this line */
-  class Vegetable {
-    constructor(Vegetable){
-      this.Vegetable = Vegetable;
+  class Vegetable{
+    constructor(name){
+      this.name=name;
     }
-
   }
-
   /* Alter code above this line */
   return Vegetable;
 }
