b953c54d1142b25b61c0cfa12d1cd418242d1fa9
DevDenver <michaelmtm2016@gmail.com>
2019-07-29 17:15:31 -0600

    Use template literals to create strings to return failure messages from function makelist

---
+++ 6-Introduction-to-the-ES6-Challenges/17-Create-Strings-using-Template-Literals.js
@@ -0,0 +1,26 @@
+/* I learned to use template Literals to create strings The syntax requires the use of backticks instead of
+quotes or double quotes to wrap the strings.  Strings can occupy lines without line break elements, but
+are not used in this example
+
+*/
+const result = {
+  success: ["max-length", "no-amd", "prefer-arrow-functions"],
+  failure: ["no-var", "var-on-top", "linebreak"],
+  skipped: ["id-blacklist", "no-dup-keys"]
+};
+function makeList(arr) {
+  "use strict";
+
+  // change code below this line
+  const resultDisplayArray = arr.map(item =>`<li class="text-warning">${item}</li>`);
+  // change code above this line
+
+  return resultDisplayArray;
+}
+/**
+ * makeList(result.failure) should return:
+ * [ `<li class="text-warning">no-var</li>`,
+ *   `<li class="text-warning">var-on-top</li>`,
+ *   `<li class="text-warning">linebreak</li>` ]
+ **/
+const resultDisplayArray = makeList(result.failure);
